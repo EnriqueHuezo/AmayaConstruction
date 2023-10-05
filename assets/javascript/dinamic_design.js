22c537512d69becb3887aa42d05a89aba5b3ed45
EnriqueHuezo <00237121@uca.edu.sv>
2023-10-05 15:25:50 -0600

    :white_check_box: index, styles and js modified

--- assets/javascript/dinamic_design.js
+++ assets/javascript/dinamic_design.js
@@ -1,15 +1,26 @@
 const hamburguer_menu = document.querySelector(".hamburguer");
-const navbar = document.querySelector(".mobile-menu");
+const navbarMobile = document.querySelector(".mobile-menu");
 let isOpen = false;
 
 hamburguer_menu.addEventListener("click", function () {
     isOpen = !isOpen;
 
     if (isOpen) {
-        navbar.classList.add('open');
+        navbarMobile.classList.add('open');
         document.body.classList.add('no-scroll');
     } else {
-        navbar.classList.remove('open');
+        navbarMobile.classList.remove('open');
         document.body.classList.remove('no-scroll');
+    }
+});
+
+
+const navbar = document.querySelector(".encabezado-normal");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY <= 0) {
+        navbar.style.backgroundColor = "transparent";
+    } else {
+        navbar.style.backgroundColor = "#121212";
     }
 });
